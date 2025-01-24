30086fc799397a6b931cc5e212bb3ce7c76c38c3
realespee <wanyamasp@gmail.com>
2025-01-24 20:23:17 +0300

    Create Job Vacancy Dictype and Its Portal Pages

---
+++ theflyingfish/the_flying_fish/doctype/job_vacancy/job_vacancy.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, Simon Wanyama and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+class JobVacancy(WebsiteGenerator):
+	pass
