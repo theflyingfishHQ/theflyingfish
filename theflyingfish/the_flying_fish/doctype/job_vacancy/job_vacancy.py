816e2f393c7370df95ff3dbfd7ffea90aeb7043f
realespee <wanyamasp@gmail.com>
2025-02-06 14:04:50 +0300

    Create Sharable Nice WebLink for Job Vacancy

--- theflyingfish/the_flying_fish/doctype/job_vacancy/job_vacancy.py
+++ theflyingfish/the_flying_fish/doctype/job_vacancy/job_vacancy.py
@@ -2,8 +2,26 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe.website.utils import find_first_image, get_html_content_based_on_type
+from frappe.utils import strip_html_tags, cint
 from frappe.website.website_generator import WebsiteGenerator
 
 class JobVacancy(WebsiteGenerator):
 	def before_save(self):
 		self.route = "job-board/{}".format(self.name)
+
+	def get_context(self, context):
+		# this is for double precaution. usually it wont reach this code if not published
+		if not cint(self.is_published):
+			raise Exception("This Vacancy has not been published yet!")
+
+		context.description = (
+			self.meta_description or self.vacancy_details[:500]
+		)
+
+		context.metatags = {
+			"name": self.vacancy_title,
+			"description": context.description,
+		}
+
+		context.metatags["image"] = self.cover_image or None
