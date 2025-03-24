d174d717ed56ba3ee798aa5f0ccb16e2fa423b38
sonali <sonali.narkar@phamos.eu>
2025-03-24 15:28:51 +0100

    remove comments

--- theflyingfish/overrides/cart.py
+++ theflyingfish/overrides/cart.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
-# License: GNU General Public License v3. See license.txt
-
 import frappe
 import frappe.defaults
 from frappe import _
